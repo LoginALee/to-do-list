1aae8e464ea3e55bd13ce77a39616d9c98064d04
LoginALee <alejandrodxwwe@hotmail.com>
2021-09-06 14:44:58 -0500

    Full image cover with text

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,10 +5,10 @@ const LandingPage = () => (
   <div className="main-container">
     <div className="image-container">
       <img src="/landing.svg" alt="landing logo" />
-    </div>
-    <div className="phrase-container">
-      <h1>If you can dream it</h1>
-      <h2>You can make it!</h2>
+      <div className="phrase-container">
+        <h1>If you can dream it</h1>
+        <h2>You can make it!</h2>
+      </div>
     </div>
   </div>
 )

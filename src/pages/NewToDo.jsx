d673f596e7e586c31b70da07c17e51d7f3df7fa8
LoginALee <alejandrodxwwe@hotmail.com>
2021-10-03 16:52:17 -0500

    Add new to do form

--- src/pages/NewToDo.jsx
+++ src/pages/NewToDo.jsx
@@ -3,10 +3,34 @@ import styles from './NewToDo.module.css'
 
 const NewToDo = () => (
   <div className={styles.mainContainer}>
-    <h1>New To Do</h1>
-    <button type="submit" className={styles.plus}>
-      +
-    </button>
+    <div className={styles.secondContainer}>
+      <div>
+        <h1>New To-Do</h1>
+        <button type="submit" className={styles.plus}>
+          +
+        </button>
+      </div>
+      <form className={styles.form}>
+        <div className={styles.formContainer}>
+          <ul className={styles.inputsContainer}>
+            <li>
+              <label htmlFor="task">To-do:</label>
+              <input
+                className={styles.input}
+                type="text"
+                name="task"
+                id="task"
+              />
+            </li>
+            <li>
+              <button type="submit" className={styles.button}>
+                Add
+              </button>
+            </li>
+          </ul>
+        </div>
+      </form>
+    </div>
   </div>
 )
 

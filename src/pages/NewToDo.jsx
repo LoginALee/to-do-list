3e9f97cc83936733a876b599c41a3716a0f30fa6
LoginALee <alejandrodxwwe@hotmail.com>
2021-10-10 14:09:31 -0500

    Add showFormHandler

--- src/pages/NewToDo.jsx
+++ src/pages/NewToDo.jsx
@@ -1,37 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './NewToDo.module.css'
 
-const NewToDo = () => (
-  <div className={styles.mainContainer}>
-    <div className={styles.secondContainer}>
-      <div>
-        <h1>New To-Do</h1>
-        <button type="submit" className={styles.plus}>
-          +
-        </button>
-      </div>
-      <form className={styles.form}>
-        <div className={styles.formContainer}>
-          <ul className={styles.inputsContainer}>
-            <li>
-              <label htmlFor="task">To-do:</label>
-              <input
-                className={styles.input}
-                type="text"
-                name="task"
-                id="task"
-              />
-            </li>
-            <li>
-              <button type="submit" className={styles.button}>
-                Add
-              </button>
-            </li>
-          </ul>
+const NewToDo = () => {
+  const [showForm, setShowForm] = useState(false)
+
+  const showFormHandler = () => {
+    setShowForm(!showForm)
+  }
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.secondContainer}>
+        <div>
+          <h1>New To-Do</h1>
+          <button
+            onClick={showFormHandler}
+            type="submit"
+            className={styles.plus}
+          >
+            +
+          </button>
         </div>
-      </form>
+        {showForm && (
+          <form className={styles.form}>
+            <div className={styles.formContainer}>
+              <ul className={styles.inputsContainer}>
+                <li>
+                  <label htmlFor="task">To-do:</label>
+                  <input
+                    className={styles.input}
+                    type="text"
+                    name="task"
+                    id="task"
+                  />
+                </li>
+                <li>
+                  <button type="submit" className={styles.button}>
+                    Add
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </form>
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default NewToDo

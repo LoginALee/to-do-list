17ed3994fb2a785abc9681eb7286cba78b7170c9
LoginALee <alejandrodxwwe@hotmail.com>
2021-10-17 16:17:09 -0500

    Add My Todo's page

--- src/pages/MyToDos.jsx
+++ src/pages/MyToDos.jsx
@@ -1,9 +1,27 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react'
 import styles from './MyToDos.module.css'
+import toDos from '../lib/fakeDB'
 
 const MyToDos = () => (
   <div className={styles.mainContainer}>
-    <h1>My to dos</h1>
+    <div>
+      <h1>My To-Dos</h1>
+    </div>
+    <div className={styles.secondContainer}>
+      {toDos.length &&
+        toDos.map((toDo) => (
+          <div key={toDo.id} className={styles.toDosElements}>
+            <h3>{toDo.name}</h3>
+            {toDo.tasks.length &&
+              toDo.tasks.map((task, i) => (
+                <ul key={i}>
+                  <li key={task.name}>{task.name}</li>
+                </ul>
+              ))}
+          </div>
+        ))}
+    </div>
   </div>
 )
 
--- src/pages/NewToDo.jsx
+++ src/pages/NewToDo.jsx
@@ -1,11 +1,50 @@
 import React, { useState } from 'react'
 import styles from './NewToDo.module.css'
+import toDos from '../lib/fakeDB'
 
 const NewToDo = () => {
   const [showForm, setShowForm] = useState(false)
+  const [form] = useState([])
+  const [numberOfTask, setNumberOfTask] = useState(1)
 
   const showFormHandler = () => {
     setShowForm(!showForm)
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    const tasks = []
+    for (let i = 0; i < e.target.length; i += 1) {
+      if (e.target[i].name.includes('task')) {
+        tasks.push({ name: e.target[i].value, createdAt: new Date() })
+      }
+    }
+    const newTodo = {
+      id: toDos.length + 1,
+      name: e.target[0].value,
+      tasks,
+    }
+    toDos.push(newTodo)
+  }
+
+  const addTask = () => {
+    setNumberOfTask((prev) => prev + 1)
+    const addTaskBtn = document.getElementById('addTaskButton')
+    const li = document.createElement('li')
+
+    const label = document.createElement('label')
+    label.textContent = 'Task: '
+    label.htmlFor = `task${numberOfTask + 1}`
+
+    const input = document.createElement('input')
+    input.className = `${styles.input}`
+    input.type = 'text'
+    input.name = `task${numberOfTask + 1}`
+    input.id = `task${numberOfTask + 1}`
+
+    li.appendChild(label)
+    li.appendChild(input)
+    addTaskBtn.before(li)
   }
 
   return (
@@ -14,28 +53,53 @@ const NewToDo = () => {
         <div>
           <h1>New To-Do</h1>
           <button
+            id="showFormBtn"
             onClick={showFormHandler}
-            type="submit"
+            type="button"
             className={styles.plus}
           >
             +
           </button>
         </div>
         {showForm && (
-          <form className={styles.form}>
+          <form onSubmit={submitForm} className={styles.form}>
             <div className={styles.formContainer}>
-              <ul className={styles.inputsContainer}>
+              <ul id="toDoList" className={styles.inputsContainer}>
                 <li>
-                  <label htmlFor="task">To-do:</label>
+                  <label htmlFor="to_do_name">To-do name:</label>
                   <input
                     className={styles.input}
                     type="text"
-                    name="task"
-                    id="task"
+                    name="to_do_name"
+                    id="to_do_name"
+                    value={form.name}
                   />
                 </li>
                 <li>
-                  <button type="submit" className={styles.button}>
+                  <label htmlFor="task1">Task:</label>
+                  <input
+                    className={styles.input}
+                    type="text"
+                    name="task1"
+                    id="task1"
+                    value={form.task1}
+                  />
+                </li>
+                <button
+                  id="addTaskButton"
+                  type="button"
+                  onClick={addTask}
+                  className={styles.plus}
+                >
+                  +
+                </button>
+                <li>
+                  <button
+                    id="submitBtn"
+                    name="submit"
+                    type="submit"
+                    className={styles.button}
+                  >
                     Add
                   </button>
                 </li>

4bf96cfd324c455a9af86e0293f31796dc05f734
LoginALee <alejandrodxwwe@hotmail.com>
2021-10-17 16:16:41 -0500

    Add fakeDB

---
+++ src/lib/fakeDB.js
@@ -0,0 +1,22 @@
+const toDos = [
+  {
+    id: 1,
+    name: 'Week to-dos',
+    tasks: [
+      {
+        name: 'Do the laundry',
+        createdAt: new Date(),
+      },
+      {
+        name: 'Go to the bank',
+        createdAt: new Date(),
+      },
+      {
+        name: 'Go to the market',
+        createdAt: new Date(),
+      },
+    ],
+  },
+]
+
+export default toDos

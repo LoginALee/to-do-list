000d77cd3d5af6d5e820bae6c12ff244754cec21
LoginALee <alejandrodxwwe@hotmail.com>
2022-04-24 13:50:39 -0500

    feat: Connect Create to-do endpoint

--- src/api/to-dos/to-dos.tsx
+++ src/api/to-dos/to-dos.tsx
@@ -1,7 +1,36 @@
-export const getAllToDos = () => {}
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { fetchFromAPI, postToAPI } from '../requests'
+
+const ROOT_URL = process.env.REACT_API_URL ?? 'http://localhost:3001'
+
+export const getAllToDos = async (token: string) => {
+  try {
+    const response = await fetchFromAPI(token, `${ROOT_URL}/todos`)
+
+    if (response.data) {
+      return response.data
+    }
+    return response
+  } catch (error) {
+    return error
+  }
+}
 
 export const getToDo = () => {}
 
-export const createToDo = () => {}
+export const createToDo = async (token: string, data: any) => {
+  try {
+    const response = await postToAPI(token, `${ROOT_URL}/todos`, data)
+
+    if (response.data) {
+      return response.data
+    }
+    return response
+  } catch (error) {
+    return error
+  }
+}
 
 export const deleteToDo = () => {}
--- src/pages/NewToDo.tsx
+++ src/pages/NewToDo.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React, { useState } from 'react'
 import styles from './NewToDo.module.css'
 import toDos from '../lib/fakeDB'
+import { createToDo } from '../api/to-dos/to-dos'
+import { useAuthState } from '../context/context'
 
 interface Itask {
   name: string
@@ -13,48 +16,60 @@ interface Itask {
 function NewToDo() {
   const [showForm, setShowForm] = useState(false)
   const [form] = useState<Itask | any>([])
+  const [todoName, setTodoName] = useState('')
   const [numberOfTask, setNumberOfTask] = useState(1)
+  const { userDetails, token } = useAuthState()
 
   const showFormHandler = () => {
     setShowForm(!showForm)
   }
 
-  const submitForm = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const submitForm = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const tasks: Itask[] = []
-    const element = e.target as unknown as HTMLInputElement
+    const data = {
+      todo: {
+        title: todoName,
+        done: false,
+      },
+    }
+    const response = await createToDo(token, data)
 
-    for (let i = 0; i < e?.target?.length; i += 1) {
-      if (element[i].name.includes('task')) {
-        tasks.push({ name: element[i].value, createdAt: new Date() })
-      }
+    if (response.id) {
+      setTodoName('')
+      alert('To-do creado correctamente')
     }
-    const newTodo = {
-      id: toDos.length + 1,
-      name: element[0].value,
-      tasks,
-    }
-    toDos.push(newTodo)
+
+    // const tasks: Itask[] = []
+    // const element = e.target as unknown as HTMLInputElement
+
+    // for (let i = 0; i < e?.target?.length; i += 1) {
+    //   if (element[i].name.includes('task')) {
+    //     tasks.push({ name: element[i].value, createdAt: new Date() })
+    //   }
+    // }
+    // const newTodo = {
+    //   id: toDos.length + 1,
+    //   name: element[0].value,
+    //   tasks,
+    // }
+    // toDos.push(newTodo)
   }
 
   const addTask = () => {
-    setNumberOfTask((prev) => prev + 1)
-    const addTaskBtn = document.getElementById('addTaskButton')
-    const li = document.createElement('li')
-
-    const label = document.createElement('label')
-    label.textContent = 'Task: '
-    label.htmlFor = `task${numberOfTask + 1}`
-
-    const input = document.createElement('input')
-    input.className = `${styles.input}`
-    input.type = 'text'
-    input.name = `task${numberOfTask + 1}`
-    input.id = `task${numberOfTask + 1}`
-
-    li.appendChild(label)
-    li.appendChild(input)
-    addTaskBtn?.before(li)
+    // setNumberOfTask((prev) => prev + 1)
+    // const addTaskBtn = document.getElementById('addTaskButton')
+    // const li = document.createElement('li')
+    // const label = document.createElement('label')
+    // label.textContent = 'Task: '
+    // label.htmlFor = `task${numberOfTask + 1}`
+    // const input = document.createElement('input')
+    // input.className = `${styles.input}`
+    // input.type = 'text'
+    // input.name = `task${numberOfTask + 1}`
+    // input.id = `task${numberOfTask + 1}`
+    // li.appendChild(label)
+    // li.appendChild(input)
+    // addTaskBtn?.before(li)
   }
 
   return (
@@ -76,18 +91,19 @@ function NewToDo() {
             <div className={styles.formContainer}>
               <ul id="toDoList" className={styles.inputsContainer}>
                 <li>
-                  <label htmlFor="to_do_name">
+                  <label htmlFor="title">
                     To-do name:
                     <input
                       className={styles.input}
                       type="text"
-                      name="to_do_name"
-                      id="to_do_name"
-                      value={form?.name}
+                      name="title"
+                      id="title"
+                      value={todoName}
+                      onChange={(e) => setTodoName(e.target.value)}
                     />
                   </label>
                 </li>
-                <li>
+                {/* <li>
                   <label htmlFor="task1">
                     Task:
                     <input
@@ -98,15 +114,15 @@ function NewToDo() {
                       value={form?.task1}
                     />
                   </label>
-                </li>
-                <button
+                </li> */}
+                {/* <button
                   id="addTaskButton"
                   type="button"
                   onClick={addTask}
                   className={styles.plus}
                 >
                   +
-                </button>
+                </button> */}
                 <li>
                   <button
                     id="submitBtn"

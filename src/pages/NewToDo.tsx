2bc300da4e3217470c913df8c71bf04bf09a39cc
LoginALee <alejandrodxwwe@hotmail.com>
2022-04-24 13:54:05 -0500

    chore: remove comments

--- src/pages/NewToDo.tsx
+++ src/pages/NewToDo.tsx
@@ -4,7 +4,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React, { useState } from 'react'
 import styles from './NewToDo.module.css'
-import toDos from '../lib/fakeDB'
 import { createToDo } from '../api/to-dos/to-dos'
 import { useAuthState } from '../context/context'
 
@@ -15,10 +14,8 @@ interface Itask {
 
 function NewToDo() {
   const [showForm, setShowForm] = useState(false)
-  const [form] = useState<Itask | any>([])
   const [todoName, setTodoName] = useState('')
-  const [numberOfTask, setNumberOfTask] = useState(1)
-  const { userDetails, token } = useAuthState()
+  const { token } = useAuthState()
 
   const showFormHandler = () => {
     setShowForm(!showForm)
@@ -38,38 +35,6 @@ function NewToDo() {
       setTodoName('')
       alert('To-do creado correctamente')
     }
-
-    // const tasks: Itask[] = []
-    // const element = e.target as unknown as HTMLInputElement
-
-    // for (let i = 0; i < e?.target?.length; i += 1) {
-    //   if (element[i].name.includes('task')) {
-    //     tasks.push({ name: element[i].value, createdAt: new Date() })
-    //   }
-    // }
-    // const newTodo = {
-    //   id: toDos.length + 1,
-    //   name: element[0].value,
-    //   tasks,
-    // }
-    // toDos.push(newTodo)
-  }
-
-  const addTask = () => {
-    // setNumberOfTask((prev) => prev + 1)
-    // const addTaskBtn = document.getElementById('addTaskButton')
-    // const li = document.createElement('li')
-    // const label = document.createElement('label')
-    // label.textContent = 'Task: '
-    // label.htmlFor = `task${numberOfTask + 1}`
-    // const input = document.createElement('input')
-    // input.className = `${styles.input}`
-    // input.type = 'text'
-    // input.name = `task${numberOfTask + 1}`
-    // input.id = `task${numberOfTask + 1}`
-    // li.appendChild(label)
-    // li.appendChild(input)
-    // addTaskBtn?.before(li)
   }
 
   return (
@@ -103,26 +68,6 @@ function NewToDo() {
                     />
                   </label>
                 </li>
-                {/* <li>
-                  <label htmlFor="task1">
-                    Task:
-                    <input
-                      className={styles.input}
-                      type="text"
-                      name="task1"
-                      id="task1"
-                      value={form?.task1}
-                    />
-                  </label>
-                </li> */}
-                {/* <button
-                  id="addTaskButton"
-                  type="button"
-                  onClick={addTask}
-                  className={styles.plus}
-                >
-                  +
-                </button> */}
                 <li>
                   <button
                     id="submitBtn"

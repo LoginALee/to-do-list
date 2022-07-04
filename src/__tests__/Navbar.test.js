8fff79dc7eaf1db6f86a151ed7e2526152a52289
LoginALee <alejandrodxwwe@hotmail.com>
2022-07-04 12:48:56 -0500

    feature add logout test for navbar

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -40,4 +40,30 @@ describe('<Navbar />', () => {
     await userEvent.click(loginBtn)
     await screen.findByText(`Welcome, ${validUser.username}!`)
   })
+
+  it('Changes to pre-login links after logout', async () => {
+    render(<App />)
+    const signInLink = await screen.findByText('Sign in')
+    userEvent.click(signInLink)
+
+    const userInput = await screen.findByLabelText('User name')
+    const passwordInput = await screen.findByLabelText('Password')
+    const loginBtn = await screen.getAllByText('Login')[1]
+
+    await fireEvent.change(userInput, { target: { value: validUser.username } })
+    await fireEvent.change(passwordInput, {
+      target: { value: validUser.password },
+    })
+    await userEvent.click(loginBtn)
+    await screen.findByText(`Welcome, ${validUser.username}!`)
+
+    const logoutBtn = await screen.findByText('Log out')
+    await userEvent.click(logoutBtn)
+
+    const signUpLink = await screen.findByText('Sign up')
+
+    expect(signUpLink).toBeDefined()
+    expect(signInLink).toBeDefined()
+
+  })
 })

95eb8236797cdce5dbe28857be44656e9dd73dc4
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2022-10-21 18:19:44 -0500

    feature: Add update status when dragg and drop

--- src/api/to-dos/to-dos.tsx
+++ src/api/to-dos/to-dos.tsx
@@ -1,8 +1,11 @@
-import { fetchFromAPI, postToAPI } from '../requests'
+import { AxiosPromise } from 'axios'
+import { fetchFromAPI, postToAPI, patchToAPI, deleteToAPI } from '../requests'
 import {
   GetAllToDosResponse,
   CreateToDoResponse,
   NewTodoData,
+  Todo,
+  UpdateToDoResponse,
 } from '../../interfaces/to-dos'
 
 const ROOT_URL = process.env.REACT_API_URL ?? 'http://localhost:3001'
@@ -18,7 +21,18 @@ export const getAllToDos = async (
   return response
 }
 
-export const getToDo = () => {}
+export const updateToDo = async (
+  token: string,
+  data: NewTodoData,
+): Promise<UpdateToDoResponse> => {
+  const response: UpdateToDoResponse = await patchToAPI(
+    token,
+    `${ROOT_URL}/todos/${data.todo.id ?? ''}}`,
+    data,
+  )
+
+  return response
+}
 
 export const createToDo = async (
   token: string,
@@ -33,4 +47,11 @@ export const createToDo = async (
   return response
 }
 
-export const deleteToDo = () => {}
+export const deleteToDo = async (
+  token: string,
+  data: Todo,
+): Promise<AxiosPromise> => {
+  const response = await deleteToAPI(token, `${ROOT_URL}/todos/${data.id}`)
+
+  return response
+}

0bd4dcb232da3ca72354d58637f0296db79ea137
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2022-12-21 16:50:16 -0600

    feature: Add edit action

--- src/pages/MyTodo.tsx
+++ src/pages/MyTodo.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-restricted-globals */
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Draggable, { DraggableData } from 'react-draggable'
 import { TiDelete } from 'react-icons/ti'
+import { BsFillPencilFill } from 'react-icons/bs'
+import { motion } from 'framer-motion'
+
 import styles from './MyTodo.module.css'
 import { updateToDo, deleteToDo } from '../api/to-dos/to-dos'
 import { useAuthState } from '../context/context'
@@ -27,11 +30,31 @@ function MyTodo({
     },
   }
   const [doneLocal, setDoneLocal] = useState<boolean>(done)
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [currentTitle, setCurrentTitle] = useState(title)
   const [deltaPosition, setDeltaPosition] = useState<{ x: number; y: number }>({
     x: doneLocal ? Number(doneRef?.current?.offsetLeft) : 0,
     y: 0,
   })
   const { token } = useAuthState()
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (currentTitle === '' || !isEditing) return
+
+      void updateToDo(token ?? '', {
+        ...toDoData,
+        todo: { ...toDoData.todo, title: currentTitle },
+      }).then((response) => {
+        if (response?.data?.id) {
+          alert('To-do updated!')
+          setCurrentTitle(currentTitle)
+        }
+      })
+    }, 3000)
+
+    return () => clearTimeout(delayDebounceFn)
+  }, [currentTitle])
 
   const setToDoPosition = (data: NewTodoData, x: number, y: number) => {
     setDeltaPosition({
@@ -76,18 +99,48 @@ function MyTodo({
   }
 
   return (
-    <Draggable position={deltaPosition} onStop={onStop}>
-      <div className={styles.paper}>
-        <div className={styles.pin}>
-          <div className={styles.shadow} />
-          <div className={styles.metal} />
-          <div className={styles.bottomCircle} />
+    <motion.div layout data-isopen={isEditing}>
+      <Draggable position={deltaPosition} onStop={onStop}>
+        <div className={styles.paper} data-isopen={isEditing}>
+          <div className={styles.columnWrapper}>
+            <div className={styles.iconsWrapper}>
+              <div className={styles.pin}>
+                <div className={styles.shadow} />
+                <div className={styles.metal} />
+                <div className={styles.bottomCircle} />
+              </div>
+              <div>
+                <BsFillPencilFill
+                  size="1em"
+                  style={{ visibility: doneLocal ? 'hidden' : 'visible' }}
+                  className={styles.edit}
+                  onClick={() => setIsEditing(!isEditing)}
+                />
+                <TiDelete
+                  size="1.3em"
+                  className={styles.trash}
+                  onClick={onDelete}
+                />
+              </div>
+            </div>
+            <div className={styles.infoWrapper}>
+              <input
+                className={styles.title}
+                type="text"
+                id="title"
+                value={currentTitle}
+                contentEditable={isEditing}
+                style={{ cursor: isEditing ? 'text' : 'grab' }}
+                onChange={(e) =>
+                  setCurrentTitle((e.target as HTMLInputElement).value)
+                }
+              />
+              <p>{`${doneLocal ? '✅' : '⏳'}`}</p>
+            </div>
+          </div>
         </div>
-        <p>{title}</p>
-        <p>{`${doneLocal ? '✅' : '⏳'}`}</p>
-        <TiDelete size="1.5em" className={styles.trash} onClick={onDelete} />
-      </div>
-    </Draggable>
+      </Draggable>
+    </motion.div>
   )
 }
 
